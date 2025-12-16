80eacf3cb978d3bc1bf37f9517d83be0f800326b
MTH <chunhau.py@gmail.com>
2025-12-16 18:40:26 +0700

    style: user sidebar added

--- app/features/user/layout/UserLayout.tsx
+++ app/features/user/layout/UserLayout.tsx
@@ -2,16 +2,21 @@ import { Outlet } from "react-router"
 
 import UserSidebar from "./UserSidebar"
 import UserHeader from "./UserHeader"
+import UserFooter from "./UserFooter"
 
 function ClientLayout() {
     return (
         <div className="min-h-screen lg:flex">
             <UserSidebar />
 
-            <main className="flex-1">
+            <main className="flex-1 flex flex-col transition-all duration-300 ease-in-out overflow-y-auto">
                 <UserHeader />
 
-                <Outlet />
+                <div className="flex-1 py-4 px-6 bg-slate-50">
+                    <Outlet />
+                </div>
+
+                <UserFooter />
             </main>
         </div>
     )
--- app/features/user/layout/UserSidebar.tsx
+++ app/features/user/layout/UserSidebar.tsx
@@ -6,8 +6,9 @@ import {
     TableIcon,
     UserCircle
 } from "lucide-react";
+import { Link } from "react-router";
 
-type NavItem = {
+type NavItemType = {
     name: string,
     icon: React.ReactNode,
     path?: string,
@@ -18,23 +19,32 @@ type NavItem = {
     }[]
 };
 
-const navItems: NavItem[] = [
+const navItems: NavItemType[] = [
     {
         icon: <GridIcon />,
-        name: "Dashboard",
+        name: "Tạo đơn ",
         path: "/dashboard"
     },
     {
         icon: <Calendar />,
-        name: "Calendar",
+        name: "Nạp tiền",
         path: "/calendar",
     },
     {
         icon: <UserCircle />,
-        name: "User Profile",
+        name: "Đơn hàng đã mua",
         path: "/profile",
     },
-
+    {
+        icon: <Calendar />,
+        name: "Bảng giá dịch vụ",
+        path: "/calendar",
+    },
+    {
+        icon: <UserCircle />,
+        name: "Tiếp thị liên kết",
+        path: "/profile",
+    },
     {
         name: "Forms",
         icon: <ListIcon />,
@@ -72,10 +82,66 @@ const navItems: NavItem[] = [
 ]
 
 function UserSidebar() {
+    const renderMenuItems = (
+        navItems: NavItemType[],
+        menuType: "main" | "others"
+    ) => {
+        return (
+            <ul className="flex flex-col gap-4">
+                {navItems.map((nav, index) => (
+                    <li key={nav.name}>
+                        {nav.subItems ? (
+                            <button>
+
+                            </button>
+                        ) : (
+                            nav.path && (
+                                <Link
+                                    className={"bg-slate-800 flex items-center w-full gap-3 px-3 py-2 font-medium rounded-lg text-[14px] group text-slate-200"}
+                                    to={nav.path}
+                                >
+                                    <span>
+                                        {nav.icon}
+                                    </span>
+                                    <span>
+                                        {nav.name}
+                                    </span>
+                                </Link>
+                            )
+                        )}
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+
     return (
-        <div>
-            UserSidebar
-        </div>
+        <aside className="sticky mt-1 lg:mt-0 flex flex-col top-0 px-5 left-0 w-72 bg-[#0f172a] text-gray-900 h-screen transition-all duration-300 ease-in-out z-50 border-r border-slate-200">
+            {/* Logo */}
+            <div className="h-18 py-3 px-6 mb-4">
+                <img 
+                    src="./images/logo.png" 
+                    alt="" 
+                    className="w-auto h-auto"
+                />
+            </div>
+
+            {/* Nav Item */}
+            <div className="flex flex-col overflow-y-auto duration-300 ease-linear no-scrollbar">
+                <nav className="mb-6">
+                    <div className="flex flex-col gap-4">
+                        {/* Menu Item */}
+                            <h2 className={`mb-4 text-xs uppercase flex leading-[20px] text-slate-400 font-medium`}>
+                                MENU
+                            </h2>
+                            {/* render menu items */}
+                            {renderMenuItems(navItems, "main")}
+
+                        {/* Service Item */}
+                    </div>
+                </nav>
+            </div>
+        </aside>
     )
 }
 
