6e93809d21190904eb18f44e522dbb15495ad01a
MTH <chunhau.py@gmail.com>
2025-12-20 17:41:52 +0700

    style: completed user info toggle in header

---
+++ app/components/ui/dropdown-item.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router";
+
+interface DropdownItemProps {
+    onItemClick: () => void;
+    children: React.ReactNode;
+    to: string
+}
+
+function DropdownItem({ onItemClick, children, to }: DropdownItemProps) {
+    return (
+        <Link to={to} className="flex items-center gap-3 px-4 py-2 font-medium text-slate-700 text=left rounded-lg group text-[14px] hover:bg-gray-100 hover:text-slate-700 cursor-pointer">
+            { children }
+        </Link>
+    )
+}
+
+export default DropdownItem;
