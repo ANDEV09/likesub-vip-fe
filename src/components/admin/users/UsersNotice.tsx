07116e16fcd0155bb7eda596928dfcea8e125c74
maaitlunghau <chunhau.py@gmail.com>
2026-01-04 22:52:26 +0700

    style(users/users-notice): changed color background

--- src/components/admin/users/UsersNotice.tsx
+++ src/components/admin/users/UsersNotice.tsx
@@ -18,7 +18,7 @@ export default function UsersNotice() {
 
     return (
         <div
-            className={`${isHidden ? "hidden" : "flex"} items-start justify-between w-full gap-2 bg-white shadow-md border-2 border-blue-200 rounded-sm p-4 h-28 ${isClosing ? "opacity-0 transition-colors duration-500 -translate-y-2" : "opacity-100 translate-y-0"}`}
+            className={`${isHidden ? "hidden" : "flex"} items-start justify-between w-full gap-2 bg-blue-100 shadow-md border-2 border-blue-200 rounded-sm p-4 h-28 ${isClosing ? "opacity-0 transition-colors duration-500 -translate-y-2" : "opacity-100 translate-y-0"}`}
         >
             <div className="text-blue-800 text-sm min-h-full flex flex-col justify-between">
                 <div className="flex items-center gap-2 text-yellow-500">
@@ -29,14 +29,14 @@ export default function UsersNotice() {
                 </div>
                 <div className="flex flex-col">
                     <span className="">
-                    Việc chỉnh sửa hoặc vô hiệu hóa người dùng có thể ảnh hưởng đến dữ liệu, đơn hàng và lịch sử giao dịch liên quan.
+                        Việc chỉnh sửa hoặc vô hiệu hóa người dùng có thể ảnh hưởng đến dữ liệu, đơn hàng và lịch sử giao dịch liên quan.
                     </span>
                     <span>
                         Vui lòng kiểm tra kỹ trước khi thực hiện.
                     </span>
                 </div>
             </div>
-            <button 
+            <button
                 onClick={() => handleHideContainer()}
             >
                 <X className="w-5 h-5 text-blue-300 hover:text-blue-400 transition-colors duration-300 cursor-pointer" />
