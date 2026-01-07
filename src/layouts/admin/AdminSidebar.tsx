68df026e95f56dd3597a75b1435bbb5f9424320e
maaitlunghau <chunhau.py@gmail.com>
2026-01-07 12:41:24 +0700

    style(admin-sidebar): updated nav items

--- src/layouts/admin/AdminSidebar.tsx
+++ src/layouts/admin/AdminSidebar.tsx
@@ -6,17 +6,18 @@ import Link from "next/link";
 
 import {
     ShoppingCart,
-    LifeBuoy,
     ChevronDownIcon,
     LayoutDashboard,
     Users,
     Package,
-    Server,
-    Wallet,
-    Link2,
     BarChart3,
-    Package2,
-    Headset
+    Headset,
+    Dot,
+    History,
+    Calendar,
+    PlusCircle,
+    Ban,
+    MessageSquareText
 } from "lucide-react";
 import { useSidebar } from "@/contexts/shared/SidebarContext";
 
@@ -27,6 +28,7 @@ interface NavItemType {
     subItems?: {
         name: string,
         path: string,
+        icon: React.ReactNode,
         new?: boolean
     }[]
 };
@@ -34,95 +36,122 @@ interface NavItemType {
 const adminMenuItems: NavItemType[] = [
     {
         name: "Dashboard",
-        icon: <LayoutDashboard size={16} />,
+        icon: <LayoutDashboard size={18} />,
         path: "/admin/dashboard",
     },
     {
-        name: "Quản lý người dùng",
-        icon: <Users size={16} />,
-        path: "/admin/users"
-    },
-    {
-        name: "Tickets",
-        icon: <Headset size={16} />,
-        path: "/admin/tickets"
-    },
-    {
-        name: "Quản lý sản phẩm",
-        icon: <Package size={16} />,
+        name: "Lịch sử",
+        icon: <History size={18} />,
         subItems: [
-            { name: "Danh sách sản phẩm", path: "/admin/products" },
-            { name: "Thêm mới sản phẩm", path: "/admin/add-product" },
+            {
+                name: "Nhật ký hoạt động",
+                path: "/admin/history/activities",
+                icon: <Dot size={18} />
+            },
+            {
+                name: "Biến động số dư",
+                path: "/admin/history/balance-fluctuations",
+                icon: <Dot size={18} />
+            },
+            {
+                name: "Nhật ký Bot Telegram",
+                path: "/admin/history/bot-telegram",
+                icon: <Dot size={18} />
+            },
         ],
     },
     {
-        name: "Quản lý đơn hàng",
-        icon: <ShoppingCart size={16} />,
-        subItems: [
-            { name: "Tất cả đơn hàng", path: "/admin/invoices" },
-            { name: "Thông tin đơn hàng", path: "/admin/invoice-detail" },
-            { name: "Thêm đơn hàng", path: "/admin/create-invoice" },
-        ],
+        name: "Tự động hoá",
+        icon: <Calendar size={18} />,
+        path: "/admin/automating"
+    },
+    {
+        name: "Addons",
+        icon: <PlusCircle size={18} />,
+        path: "/admin/addons"
+    },
+    {
+        name: "Block IP",
+        icon: <Ban size={18} />,
+        path: "/admin/automating"
+    },
+    {
+        name: "AI Chat",
+        icon: <MessageSquareText size={18} />,
+        path: "/admin/automating"
     },
 ];
 
 const adminServiceItems: NavItemType[] = [
     {
-        name: "Quản lý dịch vụ",
-        icon: <Package2 size={16} />,
+        name: "TUANORI",
+        icon: <ShoppingCart size={18} />,
         subItems: [
-            { name: "Danh sách dịch vụ", path: "/admin/services" },
-            { name: "Tạo dịch vụ mới", path: "/admin/services/create" },
-            { name: "Cấu hình giá", path: "/admin/services/pricing" },
-            { name: "Bật / Tắt dịch vụ", path: "/admin/services/status" },
-        ],
-    },
-    {
-        name: "Nhà cung cấp API",
-        icon: <Server size={16} />,
-        subItems: [
-            { name: "Danh sách API", path: "/admin/providers" },
-            { name: "Thêm API mới", path: "/admin/providers/create" },
-            { name: "Log gọi API", path: "/admin/providers/logs" },
-        ],
-    },
-    {
-        name: "Giao dịch & ví",
-        icon: <Wallet size={16} />,
-        subItems: [
-            { name: "Lịch sử nạp tiền", path: "/admin/transactions/deposit" },
-            { name: "Lịch sử chi tiêu", path: "/admin/transactions/spending" },
-            { name: "Hoàn tiền", path: "/admin/transactions/refund" },
-        ],
-    },
-    {
-        name: "Affiliate",
-        icon: <Link2 size={16} />,
-        subItems: [
-            { name: "Danh sách affiliate", path: "/admin/affiliate" },
-            { name: "Hoa hồng", path: "/admin/affiliate/commission" },
-            { name: "Thanh toán affiliate", path: "/admin/affiliate/payout" },
+            {
+                name: "Chuyên mục cha",
+                path: "/admin/services/parents",
+                icon: <Dot size={18} />
+            },
+            {
+                name: "Chuyên mục con",
+                path: "/admin/services/childs",
+                icon: <Dot size={18} />
+            },
+            {
+                name: "Gói dịch vụ",
+                path: "/admin/services/items",
+                icon: <Dot size={18} />
+            },
         ],
     },
 ];
 
-const adminSupportItems: NavItemType[] = [
+const adminManagementsItems: NavItemType[] = [
     {
-        name: "Hỗ trợ khách hàng",
-        icon: <LifeBuoy size={16} />,
+        name: "Thành viên",
+        icon: <Users size={18} />,
+        path: "/admin/users"
+    },
+    {
+        name: "Tickets",
+        icon: <Headset size={18} />,
+        path: "/admin/tickets"
+    },
+    {
+        name: "Sản phẩm",
+        icon: <Package size={18} />,
         subItems: [
-            { name: "Ticket hỗ trợ", path: "/admin/support/tickets" },
-            { name: "Ticket đang mở", path: "/admin/support/open" },
-            { name: "Ticket đã đóng", path: "/admin/support/closed" },
+            {
+                name: "Danh sách sản phẩm",
+                path: "/admin/products",
+                icon: <Dot size={18} />
+            },
+            {
+                name: "Thêm mới sản phẩm",
+                path: "/admin/add-product",
+                icon: <Dot size={18} />
+            },
         ],
     },
     {
-        name: "Báo cáo & thống kê",
-        icon: <BarChart3 size={16} />,
+        name: "Đơn hàng",
+        icon: <ShoppingCart size={18} />,
         subItems: [
-            { name: "Doanh thu", path: "/admin/reports/revenue" },
-            { name: "Người dùng", path: "/admin/reports/users" },
-            { name: "Dịch vụ bán chạy", path: "/admin/reports/services" },
+            {
+                name: "Tất cả đơn hàng",
+                path: "/admin/invoices",
+                icon: <Dot size={18} />
+            },
+            {
+                name: "Thông tin đơn hàng",
+                path: "/admin/invoice-detail",
+                icon: <Dot size={18} />
+            },
+            {
+                name: "Thêm đơn hàng",
+                path: "/admin/create-invoice",
+                icon: <Dot size={18} />
+            },
         ],
     },
 ];
@@ -132,7 +161,7 @@ function AdminSidebar() {
     const { isExpanded, isHovered, setIsHovered } = useSidebar();
 
     const [openSubmenu, setOpenSubmenu] = useState<{
-        type: "menu" | "services" | "support";
+        type: "main" | "services" | "management";
         index: number;
     } | null>(null);
 
@@ -141,10 +170,10 @@ function AdminSidebar() {
         return pathname === path;
     }, [pathname]);
 
-    const subMenuRefs = useRef<Record<string, HTMLDivElement | null>>({}); // to get real height
-    const [subMenuHeight, setSubMenuHeight] = useState<Record<string, number>>({}); // to get current height
+    const subMenuRefs = useRef<Record<string, HTMLDivElement | null>>({});
+    const [subMenuHeight, setSubMenuHeight] = useState<Record<string, number>>({});
 
-    const handleSubmenuToggle = (index: number, menuType: "menu" | "services" | "support") => {
+    const handleSubmenuToggle = (index: number, menuType: "main" | "services" | "management") => {
         setOpenSubmenu((prev) => {
             if (prev &&
                 prev.type === menuType &&
@@ -160,22 +189,21 @@ function AdminSidebar() {
         })
     }
 
-    // check if current path matches any submenu item
     useEffect(() => {
         let subMenuMatched = false;
 
-        ["menu", "services", "support"].map(type => {
-            const items = type === "menu"
+        ["main", "services", "management"].map(type => {
+            const items = type === "main"
                 ? adminMenuItems
                 : type === "services" ? adminServiceItems
-                    : adminSupportItems;
+                    : adminManagementsItems;
 
             items.map((nav, index) => {
                 if (nav.subItems) {
                     nav.subItems.forEach((subItem) => {
                         if (isActive(subItem.path)) {
                             setOpenSubmenu({
-                                type: type as "menu" | "services" | "support",
+                                type: type as "main" | "services" | "management",
                                 index
                             });
                             subMenuMatched = true;
@@ -189,7 +217,6 @@ function AdminSidebar() {
         })
     }, [pathname, isActive]);
 
-    // Set the height of the submenu items when the submenu is opened
     useEffect(() => {
         if (openSubmenu !== null) {
             const key = `${openSubmenu.type}-${openSubmenu.index}`;
@@ -205,7 +232,7 @@ function AdminSidebar() {
 
     const renderMenuItems = (
         navItems: NavItemType[],
-        menuType: "menu" | "services" | "support"
+        menuType: "main" | "services" | "management"
     ) => {
         return (
             <ul className="flex flex-col gap-2">
@@ -214,7 +241,7 @@ function AdminSidebar() {
                         {nav.subItems ? (
                             <button
                                 onClick={() => handleSubmenuToggle(index, menuType)}
-                                className={`relative hover:bg-slate-800 flex items-center w-full gap-3 px-3 py-3 font-normal rounded-lg text-[14px] group text-slate-300 cursor-pointer ${openSubmenu?.type === menuType && openSubmenu?.index === index
+                                className={`relative hover:bg-slate-800 flex items-center w-full gap-3 px-3 py-3 font-medium rounded-lg text-[16px]! group text-slate-300 cursor-pointer ${openSubmenu?.type === menuType && openSubmenu?.index === index
                                     ? "bg-slate-800 text-blue-400"
                                     : "text-slate-300 group-hover:text-slate-400"
                                     } cursor-pointer ${!isExpanded && !isHovered
@@ -253,7 +280,7 @@ function AdminSidebar() {
                         ) : (
                             nav.path && (
                                 <Link
-                                    className={`hover:bg-slate-800 flex items-center w-full gap-3 px-3 py-3 font-normal rounded-lg text-md! group text-slate-300 cursor-pointer 
+                                    className={`hover:bg-slate-800! flex items-center w-full gap-3 px-3 py-3 font-medium rounded-lg text-md! group text-slate-300 cursor-pointer text-[16px]! 
                                         ${isActive(nav.path)
                                             ? "bg-slate-800"
                                             : ""
@@ -291,17 +318,18 @@ function AdminSidebar() {
                                             : "0px",
                                 }}
                             >
-                                <ul className="mt-2 space-y-1 text-left ml-11">
+                                <ul className="mt-2 space-y-1 text-left ml-6">
                                     {nav.subItems.map(subItem => (
                                         <li key={subItem.name}>
                                             <Link
-                                                className={`hover:bg-slate-800! flex items-center w-full gap-3 px-3 py-2 h-10! font-normal rounded-lg text-[14px]! group text-slate-300 
+                                                className={`hover:bg-slate-800! flex items-center w-full gap-2 px-3 py-2 h-9! font-medium rounded-lg text-[14px]! group text-slate-300 
                                                     ${isActive(subItem.path)
                                                         ? "bg-slate-800 text-blue-400!"
                                                         : "text-slate-300 group-hover:text-slate-400"
                                                     }`}
                                                 href={subItem.path}
                                             >
+                                                {subItem.icon}
                                                 {subItem.name}
                                             </Link>
 
@@ -334,24 +362,24 @@ function AdminSidebar() {
                     <div className="flex flex-col gap-4">
                         {/* Menu Item */}
                         <h2 className={`text-xs uppercase flex leading-5 text-slate-400 font-medium`}>
-                            MENU
+                            MAIN
                         </h2>
-                        {/* render menu items */}
-                        {renderMenuItems(adminMenuItems, "menu")}
+                        {/* render main items */}
+                        {renderMenuItems(adminMenuItems, "main")}
 
                         {/* Service Item */}
                         <h2 className={`text-xs uppercase flex leading-5 text-slate-400 font-medium`}>
                             Dịch vụ
                         </h2>
-                        {/* render menu items */}
+                        {/* render main items */}
                         {renderMenuItems(adminServiceItems, "services")}
 
-                        {/* Support Item */}
+                        {/* Managements Item */}
                         <h2 className={`text-xs uppercase flex leading-5 text-slate-400 font-medium`}>
-                            Hỗ trợ
+                            Quản lý
                         </h2>
-                        {/* render menu items */}
-                        {renderMenuItems(adminSupportItems, "support")}
+                        {/* render main items */}
+                        {renderMenuItems(adminManagementsItems, "management")}
                     </div>
                 </nav>
             </div>
