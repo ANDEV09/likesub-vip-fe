236aede25e8647e3bdadaf40e8332491af2f4094
maaitlunghau <chunhau.py@gmail.com>
2026-01-12 12:52:23 +0700

    feat(block-ip): add skeleton loading UI

--- src/layouts/admin/AdminSidebar.tsx
+++ src/layouts/admin/AdminSidebar.tsx
@@ -412,7 +412,7 @@ function AdminSidebar() {
     }
 
     return (
-        <aside className="sticky mt-1 lg:mt-0 flex flex-col top-0 px-5 left-0 w-72! bg-[#0f172a] text-gray-900 border-r border-gray-200 h-screen transition-all duration-300 ease-in-out z-50">
+        <aside className="sticky mt-1 lg:mt-0 flex flex-col top-0 px-5 left-0 w-74! bg-[#0f172a] text-gray-900 border-r border-gray-200 h-screen transition-all duration-300 ease-in-out z-50">
             {/* Logo */}
             <Link href={"/"} className="h-18 py-3 px-6 mb-4">
                 <img
